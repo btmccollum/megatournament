d3551905dc53e7baed1e2515f3a208d64f435e2c
btmccollum <brad.mccollum@hey.com>
2022-08-26 21:17:15 -0400

    create membership migration

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_27_000833) do
+ActiveRecord::Schema.define(version: 2022_08_27_004150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "memberships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "team_id"
+    t.integer "role", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_memberships_on_team_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name", null: false
